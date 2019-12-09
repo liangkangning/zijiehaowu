0affb44c0bfed2a3beb77709a7e208bc157e3106
mac <635442166@qq.com>
2019-12-09 17:21:27 +0800

    标题

--- frontend/controllers/CommonController.php
+++ frontend/controllers/CommonController.php
@@ -20,6 +20,7 @@ class CommonController extends Controller
         Yii::$app->params['keyword'] = Yii::$app->params['config']['site_keyword'];
         Yii::$app->params['describe'] = Yii::$app->params['config']['site_describe'];
         Yii::$app->params['describe'] = Yii::$app->params['config']['site_describe'];
+        Yii::$app->params['describe'] = Yii::$app->params['config']['site_describe'];
     }
 
     /**
